76aef1ed0a1a003ffdf48654b40154982706baf0
Merlini Adrien <adrien.merlini@telecom-bretagne.eu>
2015-01-23 16:03:44 +0100

    Adding randomization of prime numbers

--- llvm-passes/ObfuscateZero/ObfuscateZero.cpp
+++ llvm-passes/ObfuscateZero/ObfuscateZero.cpp
@@ -21,11 +21,33 @@
 using namespace llvm;
 
 namespace {
+  using prime_type = uint32_t;
+
+static const prime_type Prime_array[] = {
+     2 ,    3 ,    5 ,    7,     11,     13,     17,     19,     23,     29,
+     31,    37,    41,    43,    47,     53,     59,     61,     67,     71,
+     73,    79,    83,    89,    97,    101,    103,    107,    109,    113,
+    127,   131,   137,   139,   149,    151,    157,    163,    167,    173,
+    179,   181,   191,   193,   197,    199,    211,    223,    227,    229,
+    233,   239,   241,   251,   257,    263,    269,    271,    277,    281,
+    283,   293,   307,   311,   313,    317,    331,    337,    347,    349,
+    353,   359,   367,   373,   379,    383,    389,    397,    401,    409,
+    419,   421,   431,   433,   439,    443,    449,    457,    461,    463,
+    467,   479,   487,   491,   499,    503,    509,    521,    523,    541,
+    547,   557,   563,   569,   571,    577,    587,    593,    599,    601,
+    607,   613,   617,   619,   631,    641,    643,    647,    653,    659,
+    661,   673,   677,   683,   691,    701,    709,    719,    727,    733,
+    739,   743,   751,   757,   761,    769,    773,    787,    797,    809,
+    811,   821,   823,   827,   829,    839,    853,    857,    859,    863,
+    877,   881,   883,   887,   907,    911,    919,    929,    937,    941,
+    947,   953,   967,   971,   977,    983,    991,    997};
+
 class ObfuscateZero : public BasicBlockPass {
   std::vector<Value *> IntegerVect;
   std::default_random_engine Generator;
 
 public:
+
   static char ID;
 
   ObfuscateZero() : BasicBlockPass(ID) {}
@@ -101,13 +123,33 @@ private:
       IntegerVect.emplace_back(&V);
   }
 
+  // Return a random prime number not equal to DifferentFrom
+  // If an error occurs returns 0
+  prime_type getPrime(prime_type DifferentFrom = 0) {
+      static std::uniform_int_distribution<prime_type> Rand(0, sizeof(Prime_array) / sizeof(prime_type));
+      size_t MaxLoop = 10;
+      prime_type Prime;
+
+      do {
+            Prime = Prime_array[Rand(Generator)];
+      } while(Prime == DifferentFrom && --MaxLoop);
+
+      if(!MaxLoop) {
+          return 0;
+      }
+
+      return Prime;
+  }
+
   Value *replaceZero(Instruction &Inst, Value *VReplace) {
     // Replacing 0 by:
     // prime1 * ((x | any1)**2) != prime2 * ((y | any2)**2)
     // with prime1 != prime2 and any1 != 0 and any2 != 0
-    using prime_type = uint32_t;
-    // FIXME : replace by dynamic generation
-    const prime_type p1 = 431, p2 = 277;
+    prime_type p1 = getPrime(),
+               p2 = getPrime(p1);
+
+    if(p2 == 0 || p1 == 0)
+        return nullptr;
 
     Type *ReplacedType = VReplace->getType(),
          *IntermediaryType = IntegerType::get(Inst.getParent()->getContext(),
