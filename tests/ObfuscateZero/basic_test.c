8df5bea2001dcd574d2e08dbd3f52921f62a775e
Romain <romain.thomas999@gmail.com>
2015-03-02 17:33:40 +0100

    Delete folders

--- tests/ObfuscateZero/basic_test.c
+++
@@ -1,9 +0,0 @@
-// RUN: clang -Xclang -load -Xclang LLVMObfuscateZero.so %s -S -emit-llvm -O2 -o %t1.ll
-// RUN: test `grep -c ' ret i32 0' %t1.ll` = 0
-
-int main(int argc, char *argv[]) {
-    int a = argc;
-    return 0;
-}
-
-
